952b185215bd5e116744ab2303554d374b0d287d
omid <omiddadvar94@gmail.com>
2024-01-08 22:42:40 +0330

    Working on Domain Layer

---
+++ Tricount.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tricount.Domain.Common.Interfaces;
+
+namespace Tricount.Domain.Common
+{
+    internal class BaseAuditableEntity : BaseEntity, IAuditableEntity
+    {
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
