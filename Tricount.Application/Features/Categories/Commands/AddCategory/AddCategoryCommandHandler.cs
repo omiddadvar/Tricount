b61828001a54d9c2bdfcad5848005841ec29e8c1
omid <omiddadvar94@gmail.com>
2024-02-17 21:54:31 +0330

    Develop : synchronized AddCategoryCommandHandler

--- Tricount.Application/Features/Categories/Commands/AddCategory/AddCategoryCommandHandler.cs
+++ Tricount.Application/Features/Categories/Commands/AddCategory/AddCategoryCommandHandler.cs
@@ -14,13 +14,14 @@ namespace Tricount.Application.Features.Categories.Commands.AddCategory
 {
     public class AddCategoryCommandHandler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<AddCategoryCommand, int>
     {
-        public Task<int> Handle(AddCategoryCommand request, CancellationToken cancellationToken)
+        public async Task<int> Handle(AddCategoryCommand request, CancellationToken cancellationToken)
         {
             var response = new BaseValuedCommandResponse();
 
-            var data = await unitOfWork.Repository<TricountCategory>().get
+            var data = mapper.Map<TricountCategory>(request);
+            data = await unitOfWork.Repository<TricountCategory, int>().AddAsync(data, cancellationToken);
 
-            await unitOfWork.Save();
+            await unitOfWork.SaveAsync(cancellationToken);
 
             response.IsSusses = true;
             response.Id = data.Id;
